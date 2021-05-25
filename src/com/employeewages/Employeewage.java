9aefb1496d618804ef814573515216364c6be460
siva209 <sivanarayanayeruva209@gmail.com>
2021-05-25 09:43:31 +0530

    [siva]Added:Using Functions

--- src/com/employeewages/Employeewage.java
+++ src/com/employeewages/Employeewage.java
@@ -1,42 +1,50 @@
 package com.employeewages;
 
 public class Employeewage {
-	public static final int IS_FULL_TIME = 1;
-	public static final int IS_PART_TIME = 2;
-	public static final int EMP_RATE_PER_HOUR = 20;
-	public static final int NUM_OF_WORKING_DAYS = 20;
-	public static final int MAX_HRS_IN_MONTH = 100;
-	
-	public static void main(String[] args) {
+	final	int IS_FULL_TIME = 1;
+	final int IS_PART_TIME = 2;
+	final int EMP_RATE_PER_HOUR = 20;
+	final int NUM_WORKING_DAYS = 20;
+	final int MAX_WORKING_HOURS = 100;
+
+//variables
 	int empHrs = 0;
-	int empwage = 0;
+	int empWage = 0;
 	int totalEmpWage = 0;
-	int totalWorkingDays = 0;
-	int totalEmpHrs = 0;
+	int totalEmpWorkingHrs = 0;
+	int totalEmpWorkingDays = 0;
 	
-	while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
-		totalWorkingDays++;
-	int attendance = (int) Math.floor(Math.random() * 10) % 3;
-	switch (attendance) {
-	case 0:
-	         System.out.println("Employee is absent");
-	         System.out.println();
-	         break;
-	case 1:
-	         System.out.println("Employee is present");
-	         System.out.println();
-	         empHrs = 8;
-	         break;
-	case 2:
-	         System.out.println("Employee is present for Part time");
-	         System.out.println();
-	         empHrs = 4;
-	         break;
-	}
-	totalEmpHrs = totalEmpHrs + empHrs;
-	System.out.println("Day: " + totalWorkingDays + "Emp Hr: " + empHrs);
-  }
-totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-System.out.println("Employee's salary for the Month is " + totalEmpWage);
+public void calculateEmpWage()
+{
+	while ( totalEmpWorkingDays <= NUM_WORKING_DAYS && totalEmpWorkingHrs < MAX_WORKING_HOURS )
+{	int empCheck = (int)(Math.random() * 10) % 3;
+
+	switch ( empCheck ){
+		case IS_FULL_TIME:
+			empHrs=8;
+			totalEmpWorkingDays++;
+			break;
+		case IS_PART_TIME:
+			empHrs = 4;
+			totalEmpWorkingDays++;
+			break;
+		default:
+			empHrs=0;
 }
+	totalEmpWorkingHrs += empHrs;
+	empWage = empHrs * EMP_RATE_PER_HOUR;
+	totalEmpWage += empWage;
+	System.out.println("Emp wage: " + empWage );
+
+}
+	System.out.println("Total number of working days: "+ totalEmpWorkingDays +" Total number of Working Hours: " + totalEmpWorkingHrs + " Total Wage: " + totalEmpWage );
+
+}	
+	public static void main(String[] args)
+{
+	Employeewage EmpWage = new Employeewage();
+	EmpWage.calculateEmpWage(); //Computation
+	
+}
+
 }
