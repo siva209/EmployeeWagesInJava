dd357285036791b428e5315b6d43345e1d12791c
siva209 <sivanarayanayeruva209@gmail.com>
2021-05-25 09:24:16 +0530

    [siva]Added:Employee wages for month

--- src/com/employeewages/Employeewage.java
+++ src/com/employeewages/Employeewage.java
@@ -1,14 +1,18 @@
 package com.employeewages;
 
 public class Employeewage {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation Problem");
-		int IS_FULL_TIME = 1;
-		int IS_PART_TIME = 2;
-		int EMP_RATE_PER_HOUR = 20;
+	
+		public static final int IS_FULL_TIME = 1;
+		public static final int IS_PART_TIME = 2;
+		public static final int EMP_RATE_PER_HOUR = 20;
+		public static final int NUM_OF_WORKING_DAYS = 20;
+		
+		public static void main(String[] args) {
 		int empHrs = 0;
 		int empwage = 0;
-		int attendance = (int) Math.round(Math.random() * 10) % 3;
+		int totalEmpWage = 0;
+		for (int i = 0; i < NUM_OF_WORKING_DAYS; i++) {
+		int attendance = (int) Math.floor(Math.random() * 10) % 3;
 		switch (attendance) {
 		case 0:
 		         System.out.println("Employee is absent");
@@ -26,6 +30,8 @@ public class Employeewage {
 		         break;
 		}
 		empwage = EMP_RATE_PER_HOUR * empHrs;
-		System.out.println("Employee's salary for the day is " + empwage);
+		totalEmpWage = totalEmpWage + empwage;
+	  }
+	System.out.println("Employee's salary for the day is " + totalEmpWage);
 	}
 }
